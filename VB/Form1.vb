f7217f3c20e0f2727ccab3cdd42b6c167ada0a6b
DevExpressExampleBot <fakeEmail>
2023-10-09 12:31:40 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,34 +1,34 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.Spreadsheet
 
 Namespace SpreadsheetImportDataTable
-	Partial Public Class Form1
-		Inherits Form
-		Private workbook As IWorkbook = Nothing
 
-		Public Sub New()
-			InitializeComponent()
-			workbook = spreadsheetControl1.Document
+    Public Partial Class Form1
+        Inherits Form
 
-			' Initialize single worksheet
-			'workbook.CreateNewDocument();
-			Dim count As Integer = workbook.Worksheets.Count
-			For i As Integer = count - 1 To 1 Step -1
-				workbook.Worksheets.RemoveAt(i)
-			Next i
-			workbook.Worksheets(0).Name = "Default"
-		End Sub
+        Private workbook As IWorkbook = Nothing
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			' Import DataTable according to the selected cell
-			Dim activeWorksheet As Worksheet = workbook.Worksheets.ActiveWorksheet
-			Dim selection As Range = activeWorksheet.SelectedCell
+        Public Sub New()
+            InitializeComponent()
+            workbook = spreadsheetControl1.Document
+            ' Initialize single worksheet
+            'workbook.CreateNewDocument();
+            Dim count As Integer = workbook.Worksheets.Count
+            For i As Integer = count - 1 To 1 Step -1
+                workbook.Worksheets.RemoveAt(i)
+            Next
 
-			spreadsheetControl1.BeginUpdate()
-			activeWorksheet.Import(ManualDataSet.CreateData().Tables(0), True, selection.TopRowIndex, selection.LeftColumnIndex)
-			spreadsheetControl1.EndUpdate()
-		End Sub
-	End Class
+            workbook.Worksheets(0).Name = "Default"
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' Import DataTable according to the selected cell
+            Dim activeWorksheet As Worksheet = workbook.Worksheets.ActiveWorksheet
+            Dim selection As Range = activeWorksheet.SelectedCell
+            spreadsheetControl1.BeginUpdate()
+            activeWorksheet.Import(ManualDataSet.CreateData().Tables(0), True, selection.TopRowIndex, selection.LeftColumnIndex)
+            spreadsheetControl1.EndUpdate()
+        End Sub
+    End Class
 End Namespace
--- VB/ManualDataSet.vb
+++ VB/ManualDataSet.vb
@@ -1,51 +1,46 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Data
 Imports System.ComponentModel
 
 Namespace SpreadsheetImportDataTable
-	Public Class ManualDataSet
-		Inherits DataSet
-		Public Sub New()
-			MyBase.New()
-			Dim table As New DataTable("table")
 
-			DataSetName = "ManualDataSet"
+    Public Class ManualDataSet
+        Inherits DataSet
 
-			table.Columns.Add("ID", GetType(Int32))
-			table.Columns.Add("MyDateTime", GetType(DateTime))
-			table.Columns.Add("MyRow", GetType(String))
-			table.Columns.Add("MyData", GetType(Double))
-			table.Constraints.Add("IDPK", table.Columns("ID"), True)
+        Public Sub New()
+            MyBase.New()
+            Dim table As DataTable = New DataTable("table")
+            DataSetName = "ManualDataSet"
+            table.Columns.Add("ID", GetType(Integer))
+            table.Columns.Add("MyDateTime", GetType(Date))
+            table.Columns.Add("MyRow", GetType(String))
+            table.Columns.Add("MyData", GetType(Double))
+            table.Constraints.Add("IDPK", table.Columns("ID"), True)
+            Tables.AddRange(New DataTable() {table})
+        End Sub
 
-			Tables.AddRange(New DataTable() { table })
-		End Sub
+        Public Shared Function CreateData() As ManualDataSet
+            Dim ds As ManualDataSet = New ManualDataSet()
+            Dim table As DataTable = ds.Tables("table")
+            table.Rows.Add(New Object() {0, Date.Today, "A", 103})
+            table.Rows.Add(New Object() {1, Date.Today, "B", 200})
+            table.Rows.Add(New Object() {2, Date.Today, "C", 446})
+            Return ds
+        End Function
 
-		Public Shared Function CreateData() As ManualDataSet
-			Dim ds As New ManualDataSet()
-			Dim table As DataTable = ds.Tables("table")
+'#Region "Disable Serialization for Tables and Relations"
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
+        Public Overloads ReadOnly Property Tables As DataTableCollection
+            Get
+                Return MyBase.Tables
+            End Get
+        End Property
 
-			table.Rows.Add(New Object() { 0, DateTime.Today, "A", 103 })
-			table.Rows.Add(New Object() { 1, DateTime.Today, "B", 200 })
-			table.Rows.Add(New Object() { 2, DateTime.Today, "C", 446 })
-
-			Return ds
-		End Function
-
-		#Region "Disable Serialization for Tables and Relations"
-		<DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
-		Public Shadows ReadOnly Property Tables() As DataTableCollection
-			Get
-				Return MyBase.Tables
-			End Get
-		End Property
-
-		<DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
-		Public Shadows ReadOnly Property Relations() As DataRelationCollection
-			Get
-				Return MyBase.Relations
-			End Get
-		End Property
-		#End Region ' Disable Serialization for Tables and Relations
-	End Class
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
+        Public Overloads ReadOnly Property Relations As DataRelationCollection
+            Get
+                Return MyBase.Relations
+            End Get
+        End Property
+'#End Region  ' Disable Serialization for Tables and Relations
+    End Class
 End Namespace
